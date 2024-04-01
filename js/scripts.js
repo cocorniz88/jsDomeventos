a5f82d37a7596d7036631ebb659bebf3cc71e394
VERGALARGA <juanguivt88@gmail.com>
2024-04-01 12:19:27 -0500

    scripts query selector#

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,18 @@ const heading = document.querySelector('.header__texto h2') // document hace ref
 console.log(heading);// nos aparece en la consola seleccionado
 
 heading.textContent = 'Nuevo Heading'; // en firefox le damos inspeccionar y nos abre una lista grande, y ahi dice textcontent que seria blog de cafe, aca lo que estamos haciendo es modificandolo a travez de JS
-heading.classList.add('nueva-clase'); //aca estamos agregando una nueva clase
+heading.classList.add('nueva-clase'); //aca estamos agregando una nueva clase, tambien con inspeccionar en firefox vemos classList
 // todo el texto y todo lo de HTML puede ser manipulado a traves de JS
 
 
-//querySelectorAll
+//querySelectorAll, a diferencia del anterior este selecciona varios elementos
+const enlaces = document.querySelectorAll('.navegacion a'); //TODOS LOS SELECTORES INICIAN CON DOCUMENT, 
+enlaces[0].textContent = 'Nuevo texto'; // para cambiar el texto del enlace seleccionado.
+enlaces[0].href = 'http://google.com';// para cambiar la direccion donde nos llevaria el enlace, se pone href por que es el enlace que vamos a modificar
+console.log(enlaces);
+//para acceder a un enlace en especifico se hace como en los array, console.log(enlaces[0])
 
+//getElementById  esta es la forma mas vieja y que poco se utiliza.
 
-
-
-//getElementById
+const heading2 = document.getElementById('heading'); // no hay necesidad de poner # por que ya se esta especificando
+console.log(heading);
