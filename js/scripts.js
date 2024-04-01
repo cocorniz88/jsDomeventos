75244f829a16ae9e2513cb2cb8382a9281d890e1
VERGALARGA <juanguivt88@gmail.com>
2024-04-01 11:00:23 -0500

    js and img

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// para seleccionara contenido hay 3 formas: 
+//queryselector: te retorna ninguno o hasta algun selector que estes escribiendo, son similiares a CSS
+//vamos a seleccionar el texto de h2 de contacto.html
+const heading = document.querySelector('h2') // document hace referencia a todo el texto de html es una funcion, todos los query inician con DOCUMENT, . por que es un objeto y el queryselector que nos va a retornar 0 o 1 elemento. dentro del parentesis va el selector de CSS y va entre comillas 
+console.log(heading);// nos aparece en la consola seleccionado
+
+
+
+
+//querySelectorAll
+
+
+
+
+//getElementById
